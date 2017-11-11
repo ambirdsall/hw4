fc7c0b7605d85bdb0109f463d62e5677628595d6
ambirdsall <ambirdsall@gmail.com>
2017-11-11 14:44:14 -0800

    Add stream-for-n-steps and funny-number-stream

--- hw4.rkt
+++ hw4.rkt
@@ -20,3 +20,18 @@
     [(> 0 n) (error "list-nth-mod: negative number")]
     [(null? nums) (error "list-nth-mod: empty list")]
     [else (car (list-tail nums (remainder n (length nums))))]))
+
+(define (stream-for-n-steps stream n)
+  (if (= n 0)
+      null
+      (let ([next-chunk (stream)])
+        (cons (car next-chunk) (stream-for-n-steps (cdr next-chunk) (- n 1))))))
+
+(define (start-funny-number-stream n)
+  (cons (if (= (modulo n 5) 0)
+            (- n)
+            n)
+        (lambda ()
+          (start-funny-number-stream (add1 n)))))
+
+(define funny-number-stream (lambda () (start-funny-number-stream 1)))
